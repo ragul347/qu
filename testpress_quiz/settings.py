cfd680e09d47918b2b0f93e2ced89bbd5b44a75e
ragul347 <60582164+ragul347@users.noreply.github.com>
2020-12-21 01:41:35 +0530

    Update settings.py

--- testpress_quiz/settings.py
+++ testpress_quiz/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT=static_dir
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 static_dir,
